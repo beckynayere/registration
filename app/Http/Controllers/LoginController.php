52ce7ce31d978c1bb3942ff1fedddc4daf18bcb0
beckynayere <rebecca.nayere@gmail.com>
2023-08-17 15:04:39 +0300

    createreturn response

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    //
+    public function check(Request $request)
+    {
+    $credentials  = $request->validate([
+        "email" =>['required', 'email'],
+        "password" => ['required'],
+
+    ]);
+    if (Auth::attempt($credentials))
+    {
+        return response() ->json([
+            'status' => true,
+            'message' => 'Success'
+        ]);
+    }
+
+    return response()->json([
+        'status' => false,
+        'message' => "fail"
+
+]);
+}
+
 }
